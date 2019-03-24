dfe02881481a193de9b5c1d846ef9cef7b8720f1
Ivan Brcko <brc_6@hotmail.com>
2019-03-24 15:11:31 +0100

    Initialize steps

---
+++ tests/Feature/Authorization.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Support\Facades\Artisan;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class Authorization extends TestCase
+{
+    public function testUser()
+    {
+
+
+    }
+}
---
+++ tests/Feature/Contacts.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class Contacts extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function testHeaders()
+    {
+        $response = $this
+            ->json('GET', route('contacts.index'), ['user_id' => 1],[
+                config('auth.apiAccess.apiKey') => config('auth.apiAccess.apiSecret'),
+                'accept' => 'application/json',
+            ]);
+
+        $response->assertStatus(404);
+
+    }
+}
